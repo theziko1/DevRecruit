cdf2dc3f94972073b5b6ad7156e3205bfd07c42d
theziko1 <zakariaghachim96@gmail.com>
2023-12-19 16:13:45 +0100

    test commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -13,21 +13,26 @@ const Navbar = () => {
       setIsMenuOpen(!isMenuOpen);
   }
   return (
-  <div className="w-[100%] h-[99px] px-[132px] border-b border-zinc-100 justify-between items-center inline-flex">
+  <div className="w-[100%] h-[99px] px-[132px] border-b border-zinc-100 justify-between items-start flex">
         <div className="justify-start items-center gap-[50px]  flex">
                   <img className="w-[150px] h-[58.85px]" src={Logo} />
-          <div className={`${isMenuOpen ? "justify-start items-center gap-[26px] flex" : "hidden"}`}>
+          <div className="justify-start items-center gap-[26px] md:flex hidden">
               <NavLink to="/" className="text-neutral-800 text-lg font-normal font-['Be Vietnam Pro'] leading-[27px]">Home</NavLink>
               <NavLink to="/Quiz" className="text-neutral-800 text-lg font-normal font-['Be Vietnam Pro'] leading-[27px]">Quiz</NavLink>
               <NavLink to="/Contact" className="text-neutral-800 text-lg font-normal font-['Be Vietnam Pro'] leading-[27px]">Contact</NavLink>
           </div>
-          <div className="sm:hidden">
+          <div className="md:hidden">
                      <button onClick={toggleMenu} className="text-NeutralDGrey focus:outline-none focus:text-gray-500">
                         {
                             isMenuOpen ?  (<FaXmark className="h-6 w-6 "/>) : (<FaBars className="h-6 w-6 "/>)
                         }
                      </button>
-                </div>
+           </div>
+           <div className={`${isMenuOpen ? "top-[100px] left-0 right-0  block bg-orange-600" : "hidden"}`}>
+              <NavLink to="/" className="block text-neutral-800 text-lg font-normal font-['Be Vietnam Pro'] leading-[27px]">Home</NavLink>
+              <NavLink to="/Quiz" className="block text-neutral-800 text-lg font-normal font-['Be Vietnam Pro'] leading-[27px]">Quiz</NavLink>
+              <NavLink to="/Contact" className="block text-neutral-800 text-lg font-normal font-['Be Vietnam Pro'] leading-[27px]">Contact</NavLink>
+          </div>
         </div>
   </div>
   )
